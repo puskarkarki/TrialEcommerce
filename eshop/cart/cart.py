c72a24cd0c837ce849e1647b0192a7220d6a25e1
puskarkarki <puskarrajkarki1234@gmail.com>
2021-09-13 15:43:26 +0545

    iteration through cart items

--- eshop/cart/cart.py
+++ eshop/cart/cart.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
-from Product.models import Products
+from Product.models import Product
 
 
 class Cart(object):
@@ -14,7 +14,7 @@ class Cart(object):
         self.session = request.session
         """get cart from the current session"""
         cart = self.session.get(settings.CART_SESSION_ID)
-        
+
         if not cart:
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
@@ -22,7 +22,7 @@ class Cart(object):
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
-        """ 
+        """
         Add a product to the cart or update it quantity
         """
         product_id = str(product.id)
@@ -49,10 +49,25 @@ class Cart(object):
 
         """
         product_id = str(product.id)
-        
+
         if product_id in self.cart:
-            
+
             del self.cart[product_id]
-            
+
             self.save()
-            
+
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products from the database
+        """
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id_in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item(['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
