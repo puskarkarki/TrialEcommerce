0c607e441972e2d4d84e1bfa44c27349cc9b7a20
puskarkarki <puskarrajkarki1234@gmail.com>
2021-09-21 16:41:05 +0545

    build cart views for adding and removing product

--- eshop/cart/views.py
+++ eshop/cart/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from Product.models import Product
+from django.views.decorators.http import require_POST
+from .cart import Cart
+from .forms import AddProductToCartForm
+from cart.forms import AddProductToCartForm
 
-# Create your views here.
+"""
+using post decorator to allow only post request
+"""
+@require_POST 
+def cart_add_product(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    """
+    We retirive the product instance via product_id and validate AddProductToCartForm
+    """
+    form = AddProductToCartForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],override_quantity=cd['override_quantity'])
+        
+    return redirect('cart:cart_detail')
+
+@require_POST
+def cart_remove_product(request, product_id):
+    cart= Cart(request)
+    product=get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart.html', {'cart': cart})
+    
