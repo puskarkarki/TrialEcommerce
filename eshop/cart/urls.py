f038c5704a393cfece60c8023b3a140a66729f07
puskarkarki <puskarrajkarki1234@gmail.com>
2021-09-21 16:49:49 +0545

    added urls

---
+++ eshop/cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.cart_add_product, name='cart_add_product'),
+    path('remove/<int:product_id>/', views.cart_remove_product,name ='cart_remove_product'),
+]
